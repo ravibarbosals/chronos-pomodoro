5439ccf3465cf8c501bacd4b56d87f9565e0a2b7
Ravi Almeida <ravialmeida10@gmail.com>
2026-02-04 13:53:46 -0300

    feat: refactor App component layout and enhance global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Este é um aplicativo React simples. Ele inclui um componente de título
-        estilizado usando CSS Modules e uma folha de estilos global que define a
-        cor do texto do título usando uma variável CSS personalizada.
-      </p>
-    </>
+      <div className='conteiner'>
+        <div className='content'>
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
+
+      <div className='conteiner'>
+        <div className='content'>
+          <section>
+            MENU
+          </section>
+        </div>
+      </div>
+
+
+      <div className='conteiner'>
+        <div className='content'>
+          <section>
+            FOOTER
+          </section>
+        </div>
+      </div>
+      </>
   );
 }
