cfbc5977fb07bcf29f468800368c17ea574e7cc4
Ravi Almeida <ravialmeida10@gmail.com>
2026-02-04 13:13:02 -0300

    feat: add lucide-react dependency and refactor App component with new styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0);
+import './styles/theme.css';
+import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
+export function App() {
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
+      <Heading>
+        Ola mundo!
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Este é um aplicativo React simples. Ele inclui um componente de título
+        estilizado usando CSS Modules e uma folha de estilos global que define a
+        cor do texto do título usando uma variável CSS personalizada.
       </p>
     </>
   );
 }
-
-export default App;
