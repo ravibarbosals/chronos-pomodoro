09be4337c99a579372d5686ff8b82fc741e29892
ravibarbosals <ravialmeida10@gmail.com>
2026-02-10 19:59:18 -0300

    feat: add utility functions for cycle type determination and time formatting; update MainForm to use new logic

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -6,13 +6,16 @@ import { useRef } from 'react';
 import type { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getnextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
   const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   const nextCycle = getnextCycle(state.currentCycle);
-  console.log(nextCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -32,8 +35,8 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: 'workTime',
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -45,7 +48,7 @@ export function MainForm() {
         activeTask: newTask,
         currentCycle: nextCycle,
         secondsRemaining,
-        formattedSecondsRemaining: '00:00',
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
         task: [...prevState.tasks, newTask],
       };
     });
