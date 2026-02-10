9b7ae5c23a9f68a67c81b1b69844cdf9a2537d20
Ravi Almeida <ravialmeida10@gmail.com>
2026-02-10 11:21:43 -0300

    feat: implement TaskContext and TaskContextProvider for state management; update App and CountDown components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
-import { useState } from 'react';
-import type { TaskStateModel } from './models/TaskStateModel';
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
 export function App() {
-  const [state, setState] = useState(initialState);
-
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
---
+++ src/contexts/TaskContext/useTaskContext.tsx
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import { TaskContext } from './TaskContext';
+
+
+
+export function useTaskContext() {
+  return useContext(TaskContext);
+}
