4155d980ef89f240f4fb06454c027479c906fd5c
Ravi Almeida <ravialmeida10@gmail.com>
2026-02-19 11:20:41 -0300

    refactor: remove unused state management and UI elements from TaskContextProvider

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 
@@ -9,30 +9,13 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  const [numero, dispatch] = useReducer((state, action) => {
-    console.log(state, action);
-
-    switch (action) {
-      case 'INCREMENT':
-        return state + 1;
-      case 'DECREMENT':
-        return state - 1;
-      case 'INITIAL_STATE':
-        return 0;
-    }
-    return state;
-  }, 0);
-
-  //useEffect(() => {
-  // console.log(state);
-  //}, [state]);
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      <h1>o numero é: {numero}</h1>
-      <button onClick={() => dispatch('INCREMENT')}>Incrementar</button>
-      <button onClick={() => dispatch('DECREMENT')}>Decrementar</button>
-      <button onClick={() => dispatch('INITIAL_STATE')}>Zerar</button>
+      {children}
     </TaskContext.Provider>
   );
 }
