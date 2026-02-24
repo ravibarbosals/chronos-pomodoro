cb0faccfe3bdead6e7979f4d5e8f14f1b328e3e5
Ravi Almeida <ravialmeida10@gmail.com>
2026-02-24 11:31:51 -0300

    feat: add task action types and reducer for task management

---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,25 @@
+import type { TaskModel } from '../../models/TaskModel';
+
+export enum TaskActionTypes {
+  START_TASK = 'START_TASK',
+  INTERRUPT_TASK = 'INTERRUPT_TASK',
+  RESET_STATE = 'RESET_STATE',
+}
+
+export type TaskActionWithPayload =
+  | {
+      type: TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionTypes.INTERRUPT_TASK;
+      payload: TaskModel;
+    };
+
+export type TaskActionWithoutPayload = {
+  type: TaskActionTypes.RESET_STATE;
+};
+
+export type TaskActionModel = 
+  | TaskActionWithPayload 
+  | TaskActionWithoutPayload;
